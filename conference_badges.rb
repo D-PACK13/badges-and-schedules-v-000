2c004c44f443c2ab69dad36400a67cccfc0ceed9
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-07 01:20:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(speakers)
   counter = 0
   speakers.map do |speaker|
-    "Hello, #{speaker}! You'll be assigned to room #{counter}!"
+    return "Hello, #{speaker}! You'll be assigned to room #{counter}!"
     counter += 1
   end
 end
