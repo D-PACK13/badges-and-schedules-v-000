c494192de19a06a938f9dbbadd10944b429f2b8b
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-07 01:34:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 def assign_rooms(speakers)
   greet = []
   speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
-  return greet
+  greet
   end
 end
