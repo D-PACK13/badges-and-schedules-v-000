dbf6e12e4fbb3d990b26494d5533ce540406826b
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-07 01:09:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(attendees)
+  attendees.collect do |badges|
+    name = badges
+    badge_maker(name) 
+  end
+end
+
+def assign_rooms(speakers)
+  counter = 0
+  speakers.collect do |speaker|
+    puts "Hello, #{speaker}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+end
